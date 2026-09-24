afc60db81d60b613bf14c2310087ebfe3d02b403
Rubal Beniwal <beniwalrubal11@gmail.com>
2026-09-24 23:16:43 +0530

    Droplet Bridge Web Portal for GitHub Pages

---
+++ sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'droplet-web-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
